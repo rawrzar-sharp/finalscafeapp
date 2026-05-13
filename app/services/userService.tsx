f2921d039146baf65d9762c4974b8c00884f3811
Shandy <shandyafrianm25@gmail.com>
2026-05-12 18:59:22 -0700

    Delete all mobile size

--- app/services/userService.tsx
+++
@@ -1,9 +0,0 @@
-let points = 0;
-
-export const UserService = {
-  getPoints: () => points,
-  earnPoints: (amount: number) => {
-    // Setiap kelipatan Rp 10.000 mendapatkan 1 poin
-    points += Math.floor(amount / 10000);
-  }
-};
